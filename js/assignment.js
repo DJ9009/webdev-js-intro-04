87d79088ac009da83f42b569697f1b859b948a5c
David Joslin <davidjoslin1990@gmail.com>
2025-01-19 01:05:08 -0500

    updated assignemtn and index js

--- js/assignment.js
+++ js/assignment.js
@@ -3,15 +3,21 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
-
+const responseEl = document.getElementById("response");
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
-
+if (isNaN(age)) {
+    responseEl.textContent = "Please enter a valid number.";
+} else if (age<18) {
+    responseEl.textContent = "You're underage";
+} else {
+    responseEl.textContent = "You're an adult";
+}
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+//if statement
+let danIsADog = false;
+
+if (danIsADog === true) {
+    console.log("WOOF");
+}
+
+let davidIsAGamer = false;
+if (davidIsAGamer === true) {
+    console.log("WIN");
+}
+//if/else statement
+let danIsADog = false;
+
+if (danIsADog === true) {
+    console.log("WOOF");
+} else {
+    console.log("Dan is a human.");
+}
+
+let davidIsAGamer = false;
+
+if (davidIsAGamer === true) {
+    console.log("cool");
+} else {
+    console.log("I am now the master.");
+}
+//if/else/if/else statement
+let healthPoints = 5;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+let ManaPoints = 10;
+
+if (ManaPoints >= 20) {
+    console.log("Good to go");
+} else if (ManaPoints < 20 && ManaPoints >= 10) {
+    
+}
